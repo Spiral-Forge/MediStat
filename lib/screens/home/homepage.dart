d64ebe5f5676b55f2e0e4b86d4cc8002822f14d5
Nitasha Dhingra <nitashadhingra612@gmail.com>
2020-11-26 22:18:16 +0530

    homepage buttons + navbar

---
+++ lib/screens/home/homepage.dart
@@ -0,0 +1,97 @@
+import 'package:dbapp/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // var themeFlag = false;
+  // final FirebaseAuth _authUser = FirebaseAuth.instance;
+  // bool loading = true;
+  // String post;
+  // bool postFlag = false;
+  // static List<dynamic> peerID = [];
+  // List fixedList = Iterable<int>.generate(peerID.length).toList();
+  // Future<FirebaseUser> getCurrentUser() {
+  //   return _authUser.currentUser();
+  // }
+
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final GlobalKey<ScaffoldState> _scaffoldKey =
+        new GlobalKey<ScaffoldState>();
+
+    return Scaffold(
+      key: _scaffoldKey,
+      body: Column(children: [
+        Expanded(
+            child: Container(
+                child: Padding(
+          padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+          child: Column(children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15.0, 42, 0, 0),
+              child: Row(children: [
+                IconButton(
+                    icon: Icon(Icons.menu),
+                    onPressed: () {
+                      _scaffoldKey.currentState.openDrawer();
+                    }),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(5.0, 0, 0, 0),
+                  child: Text(
+                    "MediStat",
+                    style: TextStyle(fontFamily: 'GoogleSans', fontSize: 23),
+                  ),
+                )
+              ]),
+            ),
+            SizedBox(
+              height: 90,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                InkResponse(
+                    onTap: () => print("coolcoolcool"),
+                    child: Container(
+                      width: 250,
+                      height: 250,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle, color: AppColors.MediBlue),
+                      child: Center(
+                        child: Text(
+                          "Get instant help",
+                          style: TextStyle(color: Colors.white, fontSize: 24),
+                        ),
+                      ),
+                    )),
+                SizedBox(height: 40),
+                InkResponse(
+                    onTap: () => print("pewpewpew"),
+                    child: Container(
+                      width: 250,
+                      height: 250,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle, color: AppColors.MediBlue),
+                      child: Center(
+                        child: Text(
+                          "Call an ambulance",
+                          style: TextStyle(color: Colors.white, fontSize: 24),
+                        ),
+                      ),
+                    )),
+              ],
+            )
+          ]),
+        )))
+      ]),
+    );
+  }
+}
